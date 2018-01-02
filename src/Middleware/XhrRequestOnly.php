5ded9ed208364e0205f228c9a1802da4b71b4a9d
Rocco Howard <rocco@hnh.digital>
2018-01-02 23:14:05 +1100

    Added xhr middleware check

---
+++ src/Middleware/XhrRequestOnly.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace HnhDigital\HelperCollection\Middleware;
+
+use Closure;
+
+class XhrRequestOnly
+{
+    /**
+     * Create a new middleware instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->ajax() && !$request->wantsJson()) {
+            return response('Unauthorized', 401);
+        }
+
+        return $next($request);
+    }
+}
