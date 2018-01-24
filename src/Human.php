424effe6383fbba76701a6784fb21fa8145491bf
Rocco Howard <rocco@hnh.digital>
2018-01-24 03:26:08 +0000

    Crude seconds conversion, only supports seconds/minutes

--- src/Human.php
+++ src/Human.php
@@ -9,7 +9,7 @@ class Human
      *
      * @var array
      */
-    public static $bytes_units = [
+    public $units_of_bytes = [
         'B',
         'KB',
         'MB',
@@ -33,11 +33,46 @@ class Human
     public function bytes($bytes, $decimals = 2, $mod = 1024)
     {
         $factor = floor((strlen($bytes) - 1) / 3);
-        $byte_name = static::$bytes_units[$factor];
+        $byte_name = $this->units_of_bytes[$factor];
         if ($mod == 1000) {
             $byte_name = str_replace('B', 'bit', $byte_name);
         }
 
         return sprintf("%.{$decimals}f %s", $bytes / pow($mod, $factor), $byte_name);
+    }
+
+    /**
+     * Convert an integer timeset to pretty.
+     *
+     * @param integer $offset
+     *
+     * @return string
+     */
+    public function timeOffset($offset)
+    {
+        $offset_whole = round($offset);
+        $offset_decimal = abs($offset - $offset_whole) * 60;
+
+        return (substr($offset, 0, 1) != '-' ? '+' : '').$offset_whole.':'.str_pad($offset_decimal, 2, '0', STR_PAD_LEFT);
+    }
+
+    /**
+     * Convert seconds to words.
+     *
+     * @param integer $seconds
+     *
+     * @return string
+     */
+    public function seconds($seconds)
+    {
+        if ($seconds < 60) {
+            $time = $seconds;
+            $name = 'second';
+        } else {
+            $time = round($seconds / 60, 0);
+            $name = 'minute';
+        }
+
+        return sprintf('%s %s', $time, str_plural($name, $time));
     }
 }
