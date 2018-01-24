ac8736be801ad68bf158de80bb39b26d6345c204
Rocco Howard <rocco@hnh.digital>
2018-01-24 03:51:09 +0000

    Fixed array_dotize

--- src/helpers.php
+++ src/helpers.php
@@ -2,8 +2,8 @@
 
 
 if (! function_exists('array_dotize')) {
-    function array_dotize($keys)
+    function array_dotize(...$keys)
     {
-        return explode('.', $keys);
+        return implode('.', $keys);
     }
 }
