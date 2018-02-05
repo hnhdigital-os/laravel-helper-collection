896acb8d1426989c78a623576a9e4bcba68b1c8a
Rocco Howard <rocco@hnh.digital>
2018-02-05 21:33:15 +0000

    Added array_it helper - provide any variable and return an array

--- src/helpers.php
+++ src/helpers.php
@@ -7,3 +7,21 @@ if (! function_exists('array_dotize')) {
         return implode('.', $keys);
     }
 }
+
+if (! function_exists('array_it')) {
+    function array_it($values, $check_values = false)
+    {
+        $values = is_array($values) ? $values : [$values];
+
+        // Check given values. If any have eloquent models, convert to their key value.
+        if ($check_values) {
+            foreach ($values as &$value) {
+                if (is_object($value) && $value instanceof \Illuminate\Database\Eloquent\Model) {
+                    $value = $value->getKey();
+                }
+            }
+        }
+
+        return $values;
+    }
+}
