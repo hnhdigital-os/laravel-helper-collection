f44b9f9678123404b85f66cff9d3e55133b35ab2
Rocco Howard <rocco@hnh.digital>
2018-02-11 12:37:16 +0000

    Added a text truncator

--- src/Human.php
+++ src/Human.php
@@ -114,4 +114,34 @@ class Human
 
         return sprintf('%s %s', $time, $period);
     }
+
+    /**
+     * Truncate a string.
+     *
+     * @param string $long_text
+     * @param integer $length
+     * @param array $options
+     *
+     * @return string
+     */
+    public function truncate($long_text, $length, $options = [])
+    {
+        $short_text = mb_substr($long_text, 0, $length);
+
+        if ($short_text === $long_text) {
+            return $long_text;
+        }
+
+        $short_text .= '...';
+
+        if (array_get($options, 'html', false)) {
+            $short_text .= ' <span class="f-10 f-w-100">(truncated)</span>';
+        }
+
+        if (array_get($options, 'html', false)) {
+            $short_text = sprintf('<span title="%s">%s</span>', htmlspecialchars($long_text), $short_text);
+        }
+
+        return $short_text;
+    }
 }
