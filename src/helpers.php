615e19514d4270ef3c3725cb5236d09cd2f3a16d
Rocco Howard <rocco@hnh.digital>
2018-02-11 12:26:06 +0000

    Work with collections not just arrays

--- src/helpers.php
+++ src/helpers.php
@@ -11,17 +11,21 @@ if (! function_exists('array_dotize')) {
 if (! function_exists('array_it')) {
     function array_it($values, $check_values = false)
     {
-        $values = is_array($values) ? $values : [$values];
+        $result = [];
 
-        // Check given values. If any have eloquent models, convert to their key value.
-        if ($check_values) {
-            foreach ($values as &$value) {
-                if (is_object($value) && $value instanceof \Illuminate\Database\Eloquent\Model) {
-                    $value = $value->getKey();
-                }
+        if (!($values instanceof \Illuminate\Database\Eloquent\Collection)) {
+            $values = is_array($values) ? $values : [$values];
+        }
+
+        foreach ($values as $value) {
+            // Check given values. If any have eloquent models, convert to their key value.
+            if ($check_values && is_object($value) && $value instanceof \Illuminate\Database\Eloquent\Model) {
+                $result[] = $value->getKey();
+            } else {
+                $result[] = $value;
             }
         }
 
-        return $values;
+        return $result;
     }
 }
