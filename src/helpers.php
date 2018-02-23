8fbc91f5de3fdfe007a203310309ceb5ea077af2
Rocco Howard <rocco@hnh.digital>
2018-02-23 01:49:53 +0000

    Updated timezone helpers to include a timezone value; Added currency/locale helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\Intl\Intl;
+
 /**
  * Converts a list of items into a dotted representation.
  */
@@ -35,6 +37,57 @@ if (! function_exists('array_it')) {
     }
 }
 
+
+/**
+ * Converts a number to locale.
+ */
+if (! function_exists('user_locale')) {
+    function user_locale()
+    {
+        return Auth()->user()->country_code;
+    }
+}
+
+if (! function_exists('locale_format_number')) {
+    function locale_format_number($money, $config = [])
+    {
+        $money->setLocale(user_locale());
+
+        return $money->format();
+    }
+}
+
+if (! function_exists('locale_currency_symbol')) {
+    function locale_currency_symbol($currency = 'USD')
+    {
+        return Intl::getCurrencyBundle()->getCurrencySymbol($currency);
+    }
+}
+
+if (! function_exists('float_format_currency')) {
+    function float_format_currency($value)
+    {
+        $value = preg_replace("/([-]?)(.*?)\xC2\xA0([0-9,.]*)/", '<span class="f-left">$2</span> $1$3', $value, -1, $count);
+        $value = preg_replace("/([-]?)(.*?)([0-9,.]+)/", '<span class="f-left">$2</span> $1$3', $value, -1, $count);
+
+        if ($count == 0) {
+            $value = preg_replace("/([-]?)([0-9,.]*)\xC2\xA0(.*?)/", '<span class="f-left">$2</span> $1$3', $value, -1, $count);
+        }
+
+        return $value;
+    }
+}
+
+/**
+ * Converts a DateTime to user's timezone.
+ */
+if (! function_exists('user_tz')) {
+    function user_tz()
+    {
+        return Auth()->user()->timezone;
+    }
+}
+
 /**
  * Converts a DateTime to user's timezone.
  */
@@ -53,9 +106,11 @@ if (! function_exists('user_timezone')) {
  * Converts a DateTime to user's timezone and specified time/date format.
  */
 if (! function_exists('user_timedate')) {
-    function user_timedate($datetime)
+    function user_timedate($datetime, $timezone = true)
     {
-        return user_timezone($datetime)->format(Auth()->user()->time_date_format);
+        $format = Auth()->user()->time_date_format;
+        $format .= timezone_format($timezone);
+        return user_timezone($datetime)->format($format);
     }
 }
 
@@ -63,9 +118,11 @@ if (! function_exists('user_timedate')) {
  * Converts a DateTime to user's timezone and specified time format.
  */
 if (! function_exists('user_time')) {
-    function user_time($datetime)
+    function user_time($datetime, $timezone = true)
     {
-        return user_timezone($datetime)->format(Auth()->user()->time_format);
+        $format = Auth()->user()->time_format;
+        $format .= timezone_format($timezone);
+        return user_timezone($datetime)->format($format);
     }
 }
 
@@ -73,8 +130,20 @@ if (! function_exists('user_time')) {
  * Converts a DateTime to user's timezone and specified date format.
  */
 if (! function_exists('user_date')) {
-    function user_date($datetime)
+    function user_date($datetime, $timezone = true)
     {
-        return user_timezone($datetime)->format(Auth()->user()->date_format);
+        $format = Auth()->user()->date_format;
+        $format .= timezone_format($timezone);
+        return user_timezone($datetime)->format($format);
+    }
+}
+
+/**
+ * Converts a DateTime to user's timezone and specified date format.
+ */
+if (! function_exists('timezone_format')) {
+    function timezone_format($return)
+    {
+        return $return ? ' T' : '';
     }
 }
