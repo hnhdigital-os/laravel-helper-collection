72fd8a150853e502bc772b294a7d9b2f0fc475aa
Rocco Howard <rocco@hnh.digital>
2018-07-30 23:21:35 +0000

    Added variation of the verify csrf that handles xhr requests correctly

---
+++ src/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,30 @@
+<?php
+namespace HnhDigital\HelperCollection\Middleware;
+
+use Closure;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as ParentVerifyCsrfToken;
+
+class VerifyCsrfToken extends ParentVerifyCsrfToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        try {
+            return parent::handle($request, $next);
+        } catch (\Exception $exception) {
+            if ($request->ajax()) {
+                header('X-FORCE_FRONTEND_REDIRECT: 1');
+                echo array_get($_SERVER, 'HTTP_REFERER', array_get($_SERVER, 'HTTP_ORIGIN', secure_url('')));
+                exit();
+            }
+            return $next($request);
+        }
+    }
+}
