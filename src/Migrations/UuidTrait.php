9a8d6bb8da7f9206dc0ee04b2f0e3b98fff2645b
Rocco Howard <rocco@hnh.digital>
2019-07-31 00:42:38 +0000

    Added a migrations UUID trait to create a trigger

---
+++ src/Migrations/UuidTrait.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace HnhDigital\HelperCollection;
+
+trait UuidTrait
+{
+    /**
+     * Create trigger.
+     *
+     * @param string $table
+     * @param string $column
+     *
+     * @return void
+     */
+    public function createUuidTrigger($table, $column)
+    {
+        DB::connection($this->connection)->unprepared(
+            'CREATE DEFINER = CURRENT_USER TRIGGER `'.$table.'_BEFORE_INSERT` BEFORE INSERT ON `'.$table.'` FOR EACH ROW '.
+            'BEGIN '.
+            'IF new.'.$column.'=\'\' OR new.'.$column.' IS NULL THEN '.
+            'SET new.'.$column.' = UUID(); '.
+            'END IF; '.
+            'END'
+        );
+    }
+
+    /**
+     * Drop trigger.
+     *
+     * @param string $table
+     * @param string $column
+     *
+     * @return void
+     */
+    public function dropUuidTrigger($table)
+    {
+        DB::connection($this->connection)->unprepared('DROP TRIGGER `'.$table.'_BEFORE_INSERT`');
+    }
+}
